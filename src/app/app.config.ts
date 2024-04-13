0199d266a82f8e3b0002e0c57e3d3b42f4a8d216
MagiTom <mag.tomala1@gmail.com>
2024-04-13 15:23:40 +0200

    fixes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getApp, initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
@@ -7,14 +7,16 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { environment } from '../environments/environment.development';
 import { routes } from './app.routes';
+import { getApps } from 'firebase/app';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes, withComponentInputBinding()), provideAnimationsAsync(), 
  importProvidersFrom([
-  provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+  provideFirebaseApp(() => !getApps().length ? initializeApp(environment.firebaseConfig) : getApp()),
   provideAuth(() => getAuth()),
   provideFirestore(() => getFirestore()),
   provideStorage(() => getStorage())
-]), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"worship-book-d9bd0","appId":"1:389653363046:web:84e2450b10dd37038bb0a7","storageBucket":"worship-book-d9bd0.appspot.com","apiKey":"AIzaSyCgTu_lkoXcOmyszpsP2CnGqJ4QBIWAtbU","authDomain":"worship-book-d9bd0.firebaseapp.com","messagingSenderId":"389653363046","measurementId":"G-7FTJ9VX0DQ"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())),
+]), importProvidersFrom(provideAuth(() => getAuth())), 
+importProvidersFrom(provideFirestore(() => getFirestore())),
 ]
 };
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,11 +1,11 @@
 export const environment = {
     firebaseConfig: {
-        apiKey: "AIzaSyCgTu_lkoXcOmyszpsP2CnGqJ4QBIWAtbU",
-        authDomain: "worship-book-d9bd0.firebaseapp.com",
-        projectId: "worship-book-d9bd0",
-        storageBucket: "worship-book-d9bd0.appspot.com",
-        messagingSenderId: "389653363046",
-        appId: "1:389653363046:web:84e2450b10dd37038bb0a7",
-        measurementId: "G-7FTJ9VX0DQ"
+        apiKey: "AIzaSyAtgP4QqfLU-u8Fu5K_LX94jE7jT-dgT3s",
+        authDomain: "kch-songs.firebaseapp.com",
+        projectId: "kch-songs",
+        storageBucket: "kch-songs.appspot.com",
+        messagingSenderId: "249656219772",
+        appId: "1:249656219772:web:2e71915399fffdc84551f9",
+        measurementId: "G-46S78138BL"
     }
   };
