00bb51889ae3cfeed4818081855b4e0f51edb7e3
Magdalena-Tomala <87173247+Magdalena-Tomala@users.noreply.github.com>
2025-02-28 22:45:47 +0100

    fix to transpose chord

--- src/app/shared/views/song-view/song-view.component.ts
+++ src/app/shared/views/song-view/song-view.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, inject } from '@angular/core';
-import * as Transposer from 'chord-transposer';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, computed, inject, input } from '@angular/core';
 import { SongDbRes, SongRes } from '../../../models/song.model';
 import { FirebaseService } from '../../../services/firebase.service';
 
@@ -15,20 +14,17 @@ import { FirebaseService } from '../../../services/firebase.service';
 })
 export class SongViewComponent{
   cdr = inject(ChangeDetectorRef);
-  @Input() columnsCount = 1;
-  @Input({required: true}) transpose!: number;
-  @Input({required: true}) set song(value: SongRes | SongDbRes){
-    this.songEl = value;
-    this.songArr = this.songEl?.text.split('\n') || [];
-  };
-  songEl!: SongRes | SongDbRes;
+  columnsCount = input(1);
+  transpose = input.required<number>();
+  song = input.required<SongRes | SongDbRes>();
   fireBaseService = inject(FirebaseService);
-  songArr!: string[];
-  test = 0;
 
-constructor() {
+  private readonly chromaticScale: string[] = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
 
-}
+  songArr = computed(() => {
+    return this.song().text.split('\n') || [];
+  });
+
 
 checkIfChords(verse:string): boolean{
   const regex = /\[(.*?)\]/g;
@@ -36,8 +32,29 @@ checkIfChords(verse:string): boolean{
 }
 
 removeSquereBrackets(verse:string){
-const song = this.songEl as SongDbRes;
 const chords = verse.replace(/\[(.*?)\]/g, '$1');
-  return Transposer.transpose(chords).up(this.transpose);  
+  return this.transposeChordLine(chords);  
+}
+
+private transposeChordLine(line: string): string {
+  const chordsOnly = line.replace(/\[|\]/g, ''); 
+  return chordsOnly.replace(/([A-G][#b]?m?(maj7|7|m7|dim|aug|sus\d|\/[^\s]*)?)/g, (match) => {
+    return this.transposeChord(match);
+  });
+}
+
+private transposeChord(chord: string): string {
+  const regex = /^([A-G][#b]?)(.*)$/; 
+  const match = chord.match(regex);
+  if (!match) return chord; 
+
+  const [, root, suffix] = match;
+  let index = this.chromaticScale.indexOf(root);
+  if (index === -1) return chord; 
+
+  const transposeValue = this.transpose();
+  const newIndex = (index + transposeValue + 12) % 12;
+
+  return this.chromaticScale[newIndex] + suffix;
 }
 }
